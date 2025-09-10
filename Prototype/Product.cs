9e9464f109893378b903dd198d92d2de579bc9a8
Marethy <khiemmm2004@gmail.com>
2025-09-10 10:47:51 +0700

    feat(creational-pattern): add prototype  pattern

---
+++ Prototype/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Product(string name, decimal price, string category): IProductPrototype
+    {
+
+        // Implement Clone()
+
+        public IProductPrototype Clone()
+        {
+            // Shallow copy (MemberwiseClone)
+            return (IProductPrototype)this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"{name} ({category}) - {price:C}";
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using Prototype;
+
+Product laptop = new Product("Gaming Laptop", 1500m, "Electronics");
+
+// Clone từ prototype
+Product laptopStudent = (Product)laptop.Clone();
+laptopStudent.Name = "Student Laptop";  
+laptopStudent.Price = 1200m;
+
+Product laptopPremium = (Product)laptop.Clone();
+laptopPremium.Name = "Premium Laptop";
+laptopPremium.Price = 2000m;
+
+Console.WriteLine(laptop);
+Console.WriteLine(laptopStudent);
+Console.WriteLine(laptopPremium);
