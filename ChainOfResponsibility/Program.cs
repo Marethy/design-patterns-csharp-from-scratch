f5ef3303e4eb7827b3a7094b26ea05600a160ee7
Marethy <khiemmm2004@gmail.com>
2025-09-11 18:43:31 +0700

    feat(behavioral-pattern): add CoR

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ChainOfResponsibility;
+
+// -----------------------
+// Client code
+// -----------------------
+string[] requests = { "A", "B", "C", "D" };
+
+// Build the chain only once: A → B → C
+var handlerA = new ConcreteHandlerA();
+var handlerB = new ConcreteHandlerB();
+var handlerC = new ConcreteHandlerC();
+
+handlerA.SetNext(handlerB).SetNext(handlerC);
+
+// Now test with requests
+foreach (var request in requests)
+{
+    Console.WriteLine($"\nClient: Who wants to handle {request}?");
+    handlerA.Handle(request); // start at the head of the chain
+}
