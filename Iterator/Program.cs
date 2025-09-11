e7d4db957def1c91dcbcadba750ffd3bbd5dc8fc
Marethy <khiemmm2004@gmail.com>
2025-09-11 19:01:26 +0700

    feat(behavioral-pattern): add iterator

---
+++ Iterator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Iterator;
+
+var numbers = new NumberCollection();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+
+var iterator = numbers.GetIterator();
+
+while (iterator.HasNext())
+{
+    Console.WriteLine(iterator.Next());
+}
+        
