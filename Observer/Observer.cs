cfa67e97465363faf02592fd96b2aadbcf1e05cb
Marethy <khiemmm2004@gmail.com>
2025-09-15 00:42:35 +0700

    feat(behavioral-pattern): add observer

---
+++ Observer/Observer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IPublisher
+{
+    void Subscribe(ISubscriber subscriber);
+    void Unsubscribe(ISubscriber subscriber);
+    void Notify(string message);
+}
+
+public interface ISubscriber
+{
+    void Update(string message);
+}
+
+public class SocialMedia : IPublisher
+{
+    private readonly HashSet<ISubscriber> _subscribers = new();
+
+    public void Subscribe(ISubscriber subscriber)
+    {
+        _subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(ISubscriber subscriber)
+    {
+        _subscribers.Remove(subscriber);
+    }
+
+    public void Notify(string message)
+    {
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.Update(message);
+        }
+    }
+}
+
+public class User : ISubscriber
+{
+    private readonly string _name;
+
+    public User(string name) => _name = name;
+
+    public void Update(string message)
+    {
+        Console.WriteLine($"{_name} received: {message}");
+    }
+}
+
---
+++ Observer/Program.cs
@@ -0,0 +1,9 @@
+﻿var subscribers = new List<ISubscriber>();
+var socialMedia = new SocialMedia();
+var user1 = new User("Alice");
+var user2 = new User("Bob");
+socialMedia.Subscribe(user1);
+socialMedia.Subscribe(user2);
+socialMedia.Notify("New post available!");
+socialMedia.Unsubscribe(user1);
+socialMedia.Notify("Another post available!");
