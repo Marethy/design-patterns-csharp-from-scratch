f56cf343517a2072c5d0c9d4914ee141c5c89a07
Marethy <khiemmm2004@gmail.com>
2025-09-14 21:52:56 +0700

    feat(behavioral-pattern): add template method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TemplateMethod;
+
+var coffee = new Coffee();
+coffee.PrepareBeverage();
+var tea = new Tea();    
+tea.PrepareBeverage();
+
+
---
+++ TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public abstract class BeveragePreparation
+    {
+        public void PrepareBeverage()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }
+        protected virtual void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+        protected virtual void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+        protected abstract void Brew();
+        protected abstract void AddCondiments();
+    }
+    public class Tea : BeveragePreparation
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+    }
+    public class Coffee : BeveragePreparation
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+    }   
+
+}
